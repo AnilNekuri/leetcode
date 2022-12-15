4c1a5232cce05192549de68e2b56ce7b288571e8
anilnekuri <anilnekuri@che>
2022-12-15 15:45:20 +0000

    Middle of the code

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class ThreeSumClosest {
     public static void main(String[] args) {
         System.out.println("Anil");
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int al = nums.length;
+        int diff = Integer.MAX_VALUE;
+        for(int i = 0; i < al-2; i ++){
+            int li = i+1;
+            int ri = al-1;
+            do{
+                int complement = nums[i]+nums[li]+nums[ri];
+                //if(complement < target && )
+            }while(li < ri & diff != 0);
+        }
+        return 0;
     }
 }
