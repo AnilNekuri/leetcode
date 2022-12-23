64484e5c78e814720586eef95dddfc2bcbd0bdc5
anilnekuri <anilnekuri@che>
2022-12-23 16:39:12 +0000

    Success code : GroupAnagrams Categorize by count https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2970/

--- GroupAnagramCategorizeByCount.java
+++ GroupAnagramCategorizeByCount.java
@@ -3,19 +3,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class GroupAnagrams {
+public class GroupAnagramCategorizeByCount {
     public static void main(String[] args) {
         String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
-        var ga = new GroupAnagrams();
+        var ga = new GroupAnagramCategorizeByCount();
         //ga.groupAnagrams(strs);
         System.out.println(ga.groupAnagrams(strs));
     }
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
         for (var str : strs) {
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String key = String.copyValueOf(chars);
+            int[] count = new int[26];
+            for(char c : str.toCharArray())
+                count[c-'a']++;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
             map.putIfAbsent(key,new ArrayList<>());
             map.get(key).add(str);
         }
