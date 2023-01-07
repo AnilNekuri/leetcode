b7bad4c3c2e270e63ddf7a859976192c73b27ab7
anilnekuri <anilnekuri@che>
2023-01-07 18:59:42 +0000

    Success code : ReorderLogFiles https://leetcode.com/problems/reorder-data-in-log-files/description/

--- ReorderLogFiles.java
+++ ReorderLogFiles.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,6 +15,30 @@ public class ReorderLogFiles {
     }
 
     public String[] reorderLogFiles(String[] logs) {
+        Comparator<String> comp = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                String[] split1 = s1.split(" ",2);
+                String[] split2 = s2.split(" ",2);
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+                if(!isDigit1 && !isDigit2){
+                    int comp = split1[1].compareTo(split2[1]);
+                    if(comp == 0)
+                        comp = split1[0].compareTo(split2[0]);
+                    return comp;
+                }else if(!isDigit1 && isDigit2){
+                    return -1;
+                }else if(isDigit1 && !isDigit2){
+                    return 1;
+                }
+                return 0;
+            }
+        };
+        Arrays.sort(logs,comp);
+        return logs;
+    }
+    public String[] reorderLogFilesMine(String[] logs) {
         List<String> digits = new ArrayList<>();
         List<String[]> letters = new ArrayList<>();
         List<String> results = new ArrayList<>();
