bf523fbeb9465ea6e7fda9298c1104b1c3823ed7
anilnekuri <anilnekuri@che>
2023-01-16 19:07:54 +0000

    Success code : Merge Two Sorted Lists https://leetcode.com/explore/interview/card/amazon/77/linked-list/2976/

--- com/anil/linkedList/MergeTwoSortedLists.java
+++ com/anil/linkedList/MergeTwoSortedLists.java
@@ -6,24 +6,40 @@ public class MergeTwoSortedLists {
         MergeTwoSortedLists m = new MergeTwoSortedLists();
         ListNode l1 = m.loadListNode(l1Arr);
         ListNode l2 = m.loadListNode(l2rArr);
-        ListNode r = m.mergeTwoLists(l1,l2);
+        ListNode r = m.mergeTwoListsMine(l1,l2);
         System.out.println(m.singleLinkedListToString(r));
     }
+    //recursive
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode head = new ListNode();
+        if(list1.val < list2.val){
+            head = list1;
+            list1 = list1.next;
+        }else{
+            head = list2;
+            list2 = list2.next;
+        }
+        head.next = mergeTwoLists(list1,list2);
+        return head;
+    }
+
+    public ListNode mergeTwoListsMine(ListNode list1, ListNode list2) {
         ListNode dummyHead = new ListNode();
         ListNode currentNode = dummyHead;
-        while(list1 != null || list2 != null){
-            int l1Val = list1 != null ? list1.val : 101;
-            int l2Val = list2 != null ? list2.val : 101;
-            if(l1Val < l2Val) {
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val) {
                 currentNode.next = list1;
-                list1 = list1.next != null? list1.next: null;
+                list1 = list1.next;
             }else {
                 currentNode.next = list2;
-                list2 = list2.next != null? list2.next: null;
+                list2 = list2.next;
             }
             currentNode = currentNode.next;
         }
+        if(list1 == null) currentNode.next = list2;
+        if(list2 == null) currentNode.next = list1;
         return dummyHead.next;
     }
     private ListNode loadListNode(int[] l1Arr){
