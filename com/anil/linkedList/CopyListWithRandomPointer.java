be3255197ffca29c68a65d72f8eb1d862a0adcbe
Anil Nekuri <anilnekuri@gmail.com>
2023-01-21 10:33:33 -0800

    Success code : Copy List with Random Pointer https://leetcode.com/problems/copy-list-with-random-pointer/description/

--- com/anil/linkedList/CopyListWithRandomPointer.java
+++ com/anil/linkedList/CopyListWithRandomPointer.java
@@ -7,6 +7,10 @@ public class CopyListWithRandomPointer{
     public static void main(String[] args) {
         CopyListWithRandomPointer c= new CopyListWithRandomPointer();
         int[][] arr = {{7,-1},{13,0},{11,4},{10,2},{1,0}};
+        Node head = c.loadArrayToLinkedList(arr);
+        //System.out.println(c.linkedListToString(head));
+        Node newHead = c.copyRandomList(head);
+        System.out.println(c.linkedListToString(newHead));
     }
 
     public Node copyRandomList(Node head) {
@@ -26,9 +30,9 @@ public class CopyListWithRandomPointer{
         curr = original;
         ptr = duplicate.next;
         while(curr!=null){
-            ptr = ptr.next;
             ptr.random = nodeMap.get(curr.random);
             curr = curr.next;
+            ptr = ptr.next;
         }
         return duplicate.next;
     }
@@ -39,6 +43,7 @@ public class CopyListWithRandomPointer{
         Node[] nodes = new Node[arr.length];
         for (int i = 0; i < arr.length; i++) {
             Node newNode = new Node(arr[i][0]);
+            nodes[i] = newNode;
             ptr.next = newNode;
             ptr = ptr.next;
         }
@@ -46,16 +51,16 @@ public class CopyListWithRandomPointer{
         ptr = dummyHead;
         for(int i = 0; i < arr.length; i++){
             ptr = ptr.next;
-            ptr.random = nodes[arr[i][1]];
+            ptr.random = arr[i][1] == -1 ? null: nodes[arr[i][1]];
         }
         return  dummyHead.next;
     }
 
-    protected String linkedListToString(ListNode l1){
+    protected String linkedListToString(Node l1){
         StringBuilder sb = new StringBuilder();
-        ListNode l1Tail = l1;
+        Node l1Tail = l1;
         do{
-            sb.append(l1Tail.val+" ");
+            sb.append("[val:"+l1Tail.val+",random:"+(l1Tail.random != null?l1Tail.random.val:null)+"]");
         }while((l1Tail = l1Tail.next) != null);
         return sb.toString();
     }
