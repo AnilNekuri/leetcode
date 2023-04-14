7083b48ad5eca809e60a091f44b216de45d2425d
Anil Nekuri <anilnekuri@gmail.com>
2023-04-14 10:46:35 -0700

    Success code : Word Search https://leetcode.com/explore/interview/card/amazon/84/recursion/2989/

--- com/anil/recursion/WordSearch.java
+++ com/anil/recursion/WordSearch.java
@@ -6,7 +6,6 @@ import java.util.Set;
 public class WordSearch {
     public static void main(String[] args) {
         char[][] board = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
-        //"ABCESEEEFS"
         //char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
         //char[][] board = {{'a','b'},{'c','d'}};
         WordSearch w = new WordSearch();
@@ -15,9 +14,42 @@ public class WordSearch {
     }
     static boolean exist = false;
     static int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-
+    static char[][] board;
+    static int BROWS;
+    static int BCOLS;
 
     public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.BROWS = board.length;
+        this.BCOLS = board[0].length;
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[r].length; c++){
+                if(backTrack(r,c,word,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean backTrack(int r, int c, String word, int index) {
+        if (index >= word.length()){
+            return true;
+        }
+        if (r < 0 || r >= BROWS || c < 0 || c >= BCOLS || word.charAt(index) != this.board[r][c]){
+            return false;
+        }
+        this.board[r][c] = '#';
+        boolean ret = false;
+        for(int d = 0; d < directions.length; d++){
+            ret = backTrack(r+directions[d][0],c+directions[d][1],word,index+1);
+            if(ret) break;
+        }
+        this.board[r][c] = word.charAt(index);
+        return ret;
+    }
+
+    public boolean existMine(char[][] board, String word) {
         exist = false;
         eleminateChars(board, word);
         char firstChar = word.charAt(0);
